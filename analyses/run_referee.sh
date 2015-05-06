d5a19600a67ae0c7742edd52f90d4a290f72d3d2
Darya Filippova <lynxoid@macLynxoid.local>
2015-05-05 20:11:06 -0400

    local scripts

--- analyses/run_referee.sh
+++ analyses/run_referee.sh
@@ -43,7 +43,7 @@ GENOME=/data/referee/aligned/human_genome.fa
 # TIMEOPT="-v"
 TIMEOPT="-lp"
 
-# /usr/bin/time $TIMEOPT $BIN -c $DIR/$FILE -t 10 -r $GENOME # > $DIR/$FILE.seq_comp.log 2>&1
+/usr/bin/time $TIMEOPT $BIN -c $DIR/$FILE -t 10 -r $GENOME --seq # > $DIR/$FILE.seq_comp.log 2>&1
 # ls -l $DIR/$FILE.*
 # time plzip -vf $DIR/$FILE.k\=* 2> $DIR/$FILE.plzip
 # python python/parse_plzip_output.py $DIR/$FILE.plzip
@@ -51,27 +51,27 @@ TIMEOPT="-lp"
 # ls -l $DIR/$FILE.*
 # /usr/bin/time -lp $BIN -c $DIR/P_aeruginosa_PAO1.10mil.sam
 
-DECOMPRESS=1
-if [ $DECOMPRESS -eq 1 ]
-then
-	plzip -fd $DIR/$FILE.offs.lz 2> /dev/null
-	plzip -fd $DIR/$FILE.edits.lz 2> /dev/null
-	plzip -fd $DIR/$FILE.*clip.lz 2> /dev/null
-	plzip -fd $DIR/$FILE.has_edits.lz 2> /dev/null
-	plzip -fd $DIR/$FILE.*.lz 2> /dev/null
-	# /usr/bin/time $TIMEOPT $BIN -d $DIR/$FILE -r $GENOME # > $DIR/$FILE.decomp.log 2>&1
-	# only compare chr, offs, cigar strings
-	# diff --suppress-common-lines -i -w -y -B <(cut -f 3,4 $DIR/$FILE) $DIR/$FILE.recovered | wc -l
+# DECOMPRESS=1
+# if [ $DECOMPRESS -eq 1 ]
+# then
+# 	plzip -fd $DIR/$FILE.offs.lz 2> /dev/null
+# 	plzip -fd $DIR/$FILE.edits.lz 2> /dev/null
+# 	plzip -fd $DIR/$FILE.*clip.lz 2> /dev/null
+# 	plzip -fd $DIR/$FILE.has_edits.lz 2> /dev/null
+# 	plzip -fd $DIR/$FILE.*.lz 2> /dev/null
+# 	# /usr/bin/time $TIMEOPT $BIN -d $DIR/$FILE -r $GENOME # > $DIR/$FILE.decomp.log 2>&1
+# 	# only compare chr, offs, cigar strings
+# 	# diff --suppress-common-lines -i -w -y -B <(cut -f 3,4 $DIR/$FILE) $DIR/$FILE.recovered | wc -l
 
-	# compare IDs
-	# echo "Compare read IDs (only for the aligned reads)"
-	# diff --suppress-common-lines -i -w -y -B <(gawk -F't' '{ if (and($2,4) == 0) print $1 }' SRR445718.2m.sam.headless) <(gawk -F't' '{ if (and($2,4) == 0) print $1 }' SRR445718.2m.sam.recovered) | wc -l
-	# achieved 0 differences
+# 	# compare IDs
+# 	# echo "Compare read IDs (only for the aligned reads)"
+# 	# diff --suppress-common-lines -i -w -y -B <(gawk -F't' '{ if (and($2,4) == 0) print $1 }' SRR445718.2m.sam.headless) <(gawk -F't' '{ if (and($2,4) == 0) print $1 }' SRR445718.2m.sam.recovered) | wc -l
+# 	# achieved 0 differences
 
-	echo "Compare first 10 columns"
-	FIN=$DIR/$FILE.headless
-	FOUT=$DIR/$FILE.recovered
-	PARAMS="--suppress-common-lines -i -w -y -B"
-	COLUMNS="\$1,\$2,\$3,\$4,\$5,\$6,\$7,\$8,\$9,\$10"
-	diff $PARAMS <(gawk -F'\t' '{ if (and($2,4) == 0) print $COLUMNS }' $FIN) <(gawk -F'\t' '{ if (and($2,4) == 0) print $COLUMNS }' $FOUT) | wc -l
-fi
+# 	echo "Compare first 10 columns"
+# 	FIN=$DIR/$FILE.headless
+# 	FOUT=$DIR/$FILE.recovered
+# 	PARAMS="--suppress-common-lines -i -w -y -B"
+# 	COLUMNS="\$1,\$2,\$3,\$4,\$5,\$6,\$7,\$8,\$9,\$10"
+# 	diff $PARAMS <(gawk -F'\t' '{ if (and($2,4) == 0) print $COLUMNS }' $FIN) <(gawk -F'\t' '{ if (and($2,4) == 0) print $COLUMNS }' $FOUT) | wc -l
+# fi
